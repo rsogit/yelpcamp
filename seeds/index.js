7e7ef5ba784a3052a638047e4ec890c1f0929ab4
Raul Oliveira <rso@ic.ufal.br>
2021-03-17 08:08:07 -0300

    adding images to the model

--- seeds/index.js
+++ seeds/index.js
@@ -24,9 +24,13 @@ const seedDB = async () => {
     
     for(let i=0; i<50; i++) {
         const random1000 = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random()*20+10)
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            price: price,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'http://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
         })
         await camp.save();
     }
